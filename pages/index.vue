f33eb2a5927edad3d62ed984e60dacb72c63d882
chenngeyi <perry564408020@gmail.com>
2025-10-24 15:09:55 +0800

    3

--- pages/index.vue
+++ pages/index.vue
@@ -43,7 +43,7 @@
     <div>is new images</div>
     <div>is new images2</div>
     <div>is new images3</div>
-    <div>test CICD 2</div>
+    <div>test CICD 3</div>
   </div>
 </template>
 <script setup lang="ts">
